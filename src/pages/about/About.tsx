8f5eaf1f5ae61422ada1456b15fad97817a694cc
Сидоров Никита <sidorov.no@reliab.tech>
2019-12-15 18:43:42 +0300

    refactor: delete Chert.js

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,39 +1,14 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React from "react";
 
 import s from "./style.module.scss";
-import { Doughnut } from "react-chartjs-2";
 
 interface Props {}
 
 const About: React.FC<Props> = () => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    let int = 0;
-    setInterval(() => {
-      int++;
-      setCount(int);
-    }, 50);
-  }, []);
 
   return (
     <div className={s.About}>
-      <Doughnut
-        data={{
-          labels: ["React", "Angular", "Vue"],
-          datasets: [
-            {
-              label: "# of Votes",
-              data: [60, 30, 10],
-              backgroundColor: ["#00d8ff", "#b52e31", "#42b883"],
-              borderWidth: 0
-            }
-          ]
-        }}
-      />
-      <div>
-        <div className={`${s.boom} ${s[count % 26]}`} />
-      </div>
+
     </div>
   );
 };
